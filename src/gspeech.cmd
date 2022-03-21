ea863fb2ab2623e142eb27c2707195bdb47067d8
kathishah <kathi.shah@gmail.com>
2022-03-21 01:33:24 +0000

    gspeech TTS

--- src/gspeech.cmd
+++ src/gspeech.cmd
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -H "Authorization: Bearer "$(gcloud auth application-default print-access-token) -H "Content-Type: application/json; charset=utf-8" \ 
-    -d @gspeech.input.json \
-    "https://texttospeech.googleapis.com/v1/text:synthesize"
+# https://cloud.google.com/text-to-speech
+
+curl -H "Authorization: Bearer "$(gcloud auth application-default print-access-token) -H "Content-Type: application/json; charset=utf-8" --data @./gspeech.input.json -o "../properties/P01-ML81883042/audio/gspeech.out.mp3" "https://texttospeech.googleapis.com/v1/text:synthesize"
 
