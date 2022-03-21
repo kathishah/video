66bd7ba0baaa20701335d7b322ac9c1bb21a4e17
kathishah <kathi.shah@gmail.com>
2022-03-21 01:36:40 +0000

    correcting gspeech api call cmd

--- src/gspeech.cmd
+++ src/gspeech.cmd
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # https://cloud.google.com/text-to-speech
+# https://cloud.google.com/text-to-speech/docs/basics
+# https://cloud.google.com/text-to-speech/docs/base64-decoding#linux
 
-curl -H "Authorization: Bearer "$(gcloud auth application-default print-access-token) -H "Content-Type: application/json; charset=utf-8" --data @./gspeech.input.json -o "../properties/P01-ML81883042/audio/gspeech.out.mp3" "https://texttospeech.googleapis.com/v1/text:synthesize"
+
+curl -H "Authorization: Bearer "$(gcloud auth application-default print-access-token) -H "Content-Type: application/json; charset=utf-8" --data @./gspeech.input.json -o "../properties/P01-ML81883042/audio/gspeech.out.json" "https://texttospeech.googleapis.com/v1/text:synthesize"
 
