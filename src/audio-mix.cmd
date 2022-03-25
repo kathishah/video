081e65ce1c1cfda8c175874b85fbbcbdae533f0e
kathishah <kathi.shah@gmail.com>
2022-03-25 19:30:33 +0000

    left padding and right padding the audio

--- src/audio-mix.cmd
+++ src/audio-mix.cmd
@@ -1,13 +1,34 @@
 #!/bin/bash
 
+usage() { echo "Usage: $0 [-p <property_name>]" 1>&2; exit 1; }
+
+while getopts ":p:" o; do
+    case "${o}" in
+        p)
+            PROPERTY=${OPTARG}
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+if [ -z "${PROPERTY}" ]; then
+    usage
+fi
+
+echo "property = ${PROPERTY}"
+PROPERTY_DIR="/home/chintan/dev/video/properties/$PROPERTY"
+
 
 # https://stackoverflow.com/questions/14498539/how-to-overlay-downmix-two-audio-files-using-ffmpeg
 
 ffmpeg \
-    -i ../properties/P01-ML81883042/audio/background.mp3 \
-    -i ../properties/P01-ML81883042/audio/gspeech.padded.mp3 \
+    -i $PROPERTY_DIR/audio/background.mp3 \
+    -i $PROPERTY_DIR/audio/gspeech.padded.mp3 \
     -filter_complex \
         "[1:a]asplit=2[sc][mix]; \
          [0:a][sc]sidechaincompress=threshold=0.003:ratio=20[bg]; \
          [bg][mix]amerge[final]" \
-    -map [final] -y out-audio-mix.mp3
+    -map [final] -y $PROPERTY_DIR/audio/out-audio-mix.mp3
--- src/process_audio.rb
+++ src/process_audio.rb
@@ -18,18 +18,25 @@ end
 
 unless property
   puts "Missing property name. See help"
+  exit 0
 end
 
 PROPERTY_NAME=property
 BASE_DIR="/home/chintan/dev/video/properties/#{PROPERTY_NAME}"
-puts BASE_DIR
-exit 0
+puts "Executing in #{BASE_DIR}"
+
+puts "1. left pad gspeech..."
+left_pad_gspeech_cmd = "ffmpeg -i #{BASE_DIR}/audio/gspeech.out.mp3 -af \"adelay=2000|2000\" -y #{BASE_DIR}/audio/gspeech.left_padded.mp3"
+left_pad_cmd_out = %x[#{left_pad_gspeech_cmd}]
+left_pad_cmd_exitstatus = $?.exitstatus
+puts "Executed left padding: #{left_pad_cmd_exitstatus}"
+
 
 # https://stackoverflow.com/questions/6239350/how-to-extract-duration-time-from-ffmpeg-output
 FFPROBE_CMD = "ffprobe -i FILE_PATH -show_entries format=duration -v quiet -of csv=\"p=0\""
-puts FFPROBE_CMD
+puts "2. finding length of audio files using: #{FFPROBE_CMD}"
 
-file_path_gspeech = "#{BASE_DIR}/audio/gspeech.out.mp3" 
+file_path_gspeech = "#{BASE_DIR}/audio/gspeech.left_padded.mp3" 
 gspeech_cmd = FFPROBE_CMD.sub(/FILE_PATH/,file_path_gspeech)
 length_gspeech=%x[#{gspeech_cmd}]
 puts "length of gspeech = #{length_gspeech}"
@@ -41,11 +48,11 @@ puts "length of background = #{length_background}"
 
 # https://superuser.com/questions/579008/add-1-second-of-silence-to-audio-through-ffmpeg
 padding = length_background.to_i - length_gspeech.to_i + 1
-puts "adding padding #{padding}"
+puts "3. Padding to be added: #{padding}"
 
 PADDING_CMD = "ffmpeg -i INPUT1 -af \"apad=pad_dur=PAD\" OUTPUT"
 padding_cmd_final = PADDING_CMD.sub(/INPUT1/,file_path_gspeech).sub(/PAD/,padding.to_s).sub(/OUTPUT/,"#{BASE_DIR}/audio/gspeech.padded.mp3")
-puts "padding: #{padding_cmd_final}"
+puts "4. Adding padding using: #{padding_cmd_final}"
 
 padding_cmd_out = %x[#{padding_cmd_final}]
 padding_cmd_exitstatus = $?.exitstatus
