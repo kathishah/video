2767a63722d921c7e2631319b9d78a7c416fdf3d
Chintan Shah <kathi.shah@gmail.com>
2022-04-01 10:14:28 -0700

    modularized the real estate data parsing code

--- src/trulia_data.rb
+++ src/trulia_data.rb
@@ -1,32 +1,47 @@
 require 'csv'
 require 'stringio'
 
+def extract(full_description)
+  #add more keywords here
+  keywords = ['hardwood', 'open floor', 'designer', 'ceilings', 'chef', 'pool', 'french', 'granite', 'open space'] 
+  expression = keywords.join("|")
+
+  #output
+  extracted_description = StringIO.new
+
+  #divide into sentences using period
+  sentences = full_description.split('.')
+  puts "extract(): sentences = #{sentences.length}"
+
+  #return if there are no sentences
+  return extracted_description.string if sentences.length == 1
+
+  #capture the first sentence directly
+  extracted_description << sentences[0] << ". "
+
+  for i in 1..(sentences.length - 1) do
+    if sentences[i].downcase.match(expression)
+      puts "Found match: #{sentences[i]}"
+      extracted_description << sentences[i] << ". "
+    end
+  end
+
+  puts "extract(): captured = #{extracted_description.string}"
+  extracted_description.string
+end
+
+#main
 POS_DESCRIPTION=4
-keywords = ['hardwood', 'open floor', 'designer', 'ceilings', 'chef', 'pool', 'french', 'granite', 'open space']
-expression = keywords.join("|")
 n = 5
 IO.foreach(Dir::home + "/dev/video/properties/trulia_dataset.csv") do |line|
-  if n > 0
-    CSV.parse(line) { |row|
-      extracted_description = StringIO.new
-      puts "desc = #{row[POS_DESCRIPTION]}"
-      next if n == 5
+  break if n == 0 #do only for n lines
+  CSV.parse(line) { |row|
+    puts "desc = #{row[POS_DESCRIPTION]}"
 
-      sentences = row[POS_DESCRIPTION].split('.')
-      puts "sentences = #{sentences.length}"
+    output = self.extract(row[POS_DESCRIPTION])
 
-      extracted_description << sentences[0] << ". "
-
-      for i in 1..(sentences.length - 1) do
-        if sentences[i].downcase.match(expression)
-          puts "Found match: #{sentences[i]}"
-          extracted_description << sentences[i] << ". "
-        end
-      end
-
-      puts "extracted = #{extracted_description.string}"
-      puts "========================================="
-    }
-  end
+    puts "output = #{output}"
+    puts "========================================="
+  }
   n = n - 1
 end
